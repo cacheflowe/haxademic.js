95b4e38a4b06c82567067e6ba788a523a83d23ca
Justin Gitlin <cacheflowe@cacheflowe.local>
2017-01-31 16:39:35 -0700

    Add extended/styled console methods

---
+++ dist/console.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.todo = function (msg) {
+  console.log('%c %s %s %s ', 'color: yellow; background-color: black;', '--', msg, '--');
+};
+
+console.important = function (msg) {
+  console.log('%c%s %s %s', 'color: brown; font-weight: bold; text-decoration: underline;', '--', msg, '--');
+};
