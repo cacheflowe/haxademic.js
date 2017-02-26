6005d9aaf7acae627bcca4b69d4b6e4a43acc74c
cacheflowe <cacheflowe@cacheflowe.com>
2017-02-25 20:36:44 -0700

    Add some notes to source

--- src/ease-to-value-callback.es6.js
+++ src/ease-to-value-callback.es6.js
@@ -1,6 +1,7 @@
 class EaseToValueCallback {
 
   constructor(value = 0, easeFactor = 10, callback = EaseToValueCallback.noop, finishRange = 0.01 ) {
+    if(typeof EasingFloat === 'undefined') return console.warn('EaseToValueCallback requires EasingFloat');
     this.easingFloat = new EasingFloat( value, easeFactor );
     this.callback = callback;
     this.finishRange = finishRange;
@@ -13,12 +14,11 @@ class EaseToValueCallback {
 
   easeToTarget(){
     this.callback(this.easingFloat.update());
-    // keep easing if we're not close enough
-    if( Math.abs(this.easingFloat.value() - this.easingFloat.target() ) > this.finishRange) {
+    if( Math.abs(this.easingFloat.value() - this.easingFloat.target() ) > this.finishRange) {   // keep easing if we're not close enough
       requestAnimationFrame(() => { this.easeToTarget(); });
     } else {
       this.easingFloat.setValue( this.easingFloat.target() );
-      this.callback(this.easingFloat.value()); // call the callback one last time with the final value
+      this.callback(this.easingFloat.value());                                                  // call the callback one last time with the final value
     }
   }
 }
