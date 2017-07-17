b87d8faf53ddf5decf4931928c9c235b827b09ef
Justin Gitlin <cacheflowe@cacheflowe.local>
2017-07-17 16:15:17 -0700

    Add remove array element method

--- src/array-util.es6.js
+++ src/array-util.es6.js
@@ -1,5 +1,10 @@
 class ArrayUtil {
 
+  static remove(array, element) {
+    const index = array.indexOf(element);
+    if (index !== -1) array.splice(index, 1);
+  }
+
   static shuffleArray(array) {
     array.sort(() => {return 0.5 - Math.random()});
   }
@@ -9,7 +14,7 @@ class ArrayUtil {
   }
 
   static uniqueArray(array) {
-    return array.filter((el, i, arr) => { 
+    return array.filter((el, i, arr) => {
       return arr.indexOf(el) === i;   // only return the first instance of an element
     });
   }
