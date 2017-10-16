809dbfca0653ca721af5d1fa5612bca2fb9677cb
cacheflowe <cacheflowe@cacheflowe.com>
2017-10-16 13:30:33 -0600

    Remove /dist. Let others transpile

--- dist/console.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.todo = function (msg) {
-  console.log('%c %s %s %s ', 'color: yellow; background-color: black;', '--', msg, '--');
-};
-
-console.important = function (msg) {
-  console.log('%c%s %s %s', 'color: brown; font-weight: bold; text-decoration: underline;', '--', msg, '--');
-};
