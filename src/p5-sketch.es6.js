10d228219b5b61fb82b1ba0ede4c45344c713ffe
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-06-17 15:38:54 -0600

    Add postprocessing shader example to P5Sketch

--- src/p5-sketch.es6.js
+++ src/p5-sketch.es6.js
@@ -23,7 +23,7 @@ class P5Sketch extends p5 {
   }
 
   setup() {
-    this.createCanvas(this.elSize.width, this.elSize.height);
+    this.createCanvas(this.elSize.width, this.elSize.height, p5.prototype.WEBGL);
     this.el.appendChild(this.canvas);
     this.background(0);
     this.setupFirstFrame();
@@ -56,3 +56,17 @@ class P5Sketch extends p5 {
   }
 
 }
+
+P5Sketch.defaultVertShader = `
+  // vertex shader
+  attribute vec3 aPosition;
+  attribute vec2 aTexCoord;
+  varying vec2 vTexCoord;
+  void main() {
+    vTexCoord = aTexCoord;
+    vec4 positionVec4 = vec4(aPosition, 1.0);
+    positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+    // positionVec4.xy = positionVec4.xy - 0.5;
+    gl_Position = positionVec4;
+  }
+`;
