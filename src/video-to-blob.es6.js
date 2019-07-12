bd4643fad30d5a561e6e9956e2852f069e4e5b63
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-07-11 23:26:25 -0600

    Add VideoToBlob

---
+++ src/video-to-blob.es6.js
@@ -0,0 +1,56 @@
+class VideoToBlob {
+
+  constructor(videoURL, callback) {
+    this.callback = callback;
+    this.loadVideo(videoURL);
+  }
+
+  loadFile(url, fileLoadCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.send();
+    xhr.onload = function() {
+      if (xhr.status !== 200) {
+        alert('Unexpected status code ' + xhr.status + ' for ' + url);
+        return false;
+      }
+      fileLoadCallback(new Uint8Array(xhr.response));
+    };
+  }
+
+  loadVideo(videoURL) {
+    let videoType = (videoURL.match(/mp4/gi)) ? 'video/mp4' : 'video/webm';
+    this.loadFile(videoURL, (uInt8Array) => {
+      var blob = new Blob([uInt8Array], {
+        type: videoType
+      });
+
+      let videoEl = document.createElement('video');
+      videoEl.defaultMuted = true;
+      videoEl.setAttribute('muted', "true");
+      videoEl.setAttribute('preload', "auto");
+      videoEl.setAttribute('playsinline', "true");
+      videoEl.setAttribute('crossOrigin', "anonymous");
+      videoEl.setAttribute('loop', "true");
+      // videoEl.setAttribute('autoplay', "true");
+
+      videoEl.src = URL.createObjectURL(blob);
+      videoEl.setAttribute('muted', "true");
+      videoEl.muted = true;
+      videoEl.volume = 0;
+
+      this.callback(videoEl);
+
+      // this.buildVideoTexture();
+    });
+  }
+
+  // buildVideoTexture() {
+  //   var texture = PIXI.Texture.from(this.videoEl);
+  //   this.sprite = new PIXI.Sprite(texture);
+  //   this.container.addChild(this.sprite);
+  //   this.sprite.mask = this.maskGraphics;
+  // }
+
+}
