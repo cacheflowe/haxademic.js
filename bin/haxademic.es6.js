351d736371e07c287558204c1cb1cda3245ce610
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-08-27 15:31:19 -0600

    Fix appstore and p5 objects in compiled library

--- bin/haxademic.es6.js
+++ bin/haxademic.es6.js
@@ -1,3 +1,61 @@
+class AppStore {
+
+  constructor() {
+    this.state = {};
+    this.listeners = [];
+    this.methods = {};
+    window._store = this;
+  }
+
+  addListener(obj, key) {
+    if(key) {
+      if(!this.methods[key]) this.methods[key] = [];
+      this.methods[key].push(obj);
+    } else {
+      this.listeners.push(obj);
+    }
+  }
+
+  removeListener(obj, key) {
+    if(key) {
+      if(this.methods[key]) {
+        const index = this.methods[key].indexOf(obj);
+        if (index !== -1) this.methods[key].splice(index, 1);
+      }
+    } else {
+      const index = this.listeners.indexOf(obj);
+      if (index !== -1) this.listeners.splice(index, 1);
+    }
+  }
+
+  set(key, value) {
+    if(typeof key === "undefined") throw new Error('AppStore requires legit keys');
+    this.state[key] = value;
+    this.listeners.forEach((el, i, arr) => {
+      el.storeUpdated(key, value);
+    });
+    // specific listener methods
+    const objs = this.methods[key];
+    if(objs) {
+      objs.forEach((el) => {
+        if(el[key]) el[key](value);
+        else throw new Error('AppStore listener has no callback: ' + key);
+      });
+    }
+  }
+
+  get(key) {
+    return this.state[key];
+  }
+
+  log() {
+    for(let key in _store.state) {
+      console.log(key, _store.state[key]);
+    }
+  }
+
+}
+
 class AppStoreDebug {
 
   constructor(showing=false) {
@@ -92,64 +150,6 @@ class AppStoreDistributed extends AppStore {
 
   broadcastJson(obj) {
     this.solidSocket.sendMessage(JSON.stringify(obj));
-  }
-
-}
-
-class AppStore {
-
-  constructor() {
-    this.state = {};
-    this.listeners = [];
-    this.methods = {};
-    window._store = this;
-  }
-
-  addListener(obj, key) {
-    if(key) {
-      if(!this.methods[key]) this.methods[key] = [];
-      this.methods[key].push(obj);
-    } else {
-      this.listeners.push(obj);
-    }
-  }
-
-  removeListener(obj, key) {
-    if(key) {
-      if(this.methods[key]) {
-        const index = this.methods[key].indexOf(obj);
-        if (index !== -1) this.methods[key].splice(index, 1);
-      }
-    } else {
-      const index = this.listeners.indexOf(obj);
-      if (index !== -1) this.listeners.splice(index, 1);
-    }
-  }
-
-  set(key, value) {
-    if(typeof key === "undefined") throw new Error('AppStore requires legit keys');
-    this.state[key] = value;
-    this.listeners.forEach((el, i, arr) => {
-      el.storeUpdated(key, value);
-    });
-    // specific listener methods
-    const objs = this.methods[key];
-    if(objs) {
-      objs.forEach((el) => {
-        if(el[key]) el[key](value);
-        else throw new Error('AppStore listener has no callback: ' + key);
-      });
-    }
-  }
-
-  get(key) {
-    return this.state[key];
-  }
-
-  log() {
-    for(let key in _store.state) {
-      console.log(key, _store.state[key]);
-    }
   }
 
 }
@@ -2238,66 +2238,68 @@ class Oscillations {
 
 }
 
-class P {}
-P.p = null;
-window.p = null;
+if(window.p5) {
+  class P {}
+  P.p = null;
+  window.P = P;
+  window.p = null;
 
-class P5Sketch extends p5 {
+  class P5Sketch extends p5 {
 
-  constructor(el=document.body, id='p5') {
-    super(p => {
-      // do any setup in here that needs to happen before the sketch starts
-      // (e.g. create event handlers)
-      // `p` refers to the instance that becomes `this` after the super() call
-    });
-    P.p = window.p = this;
-    this.setup = this.setup.bind(this);
-    this.draw = this.draw.bind(this);
-    // this.start = this.start.bind(this);
-    // this.stop = this.stop.bind(this);
-    this.windowResized = this.windowResized.bind(this);
+    constructor(el=document.body, id='p5') {
+      super(p => {
+        // do any setup in here that needs to happen before the sketch starts
+        // (e.g. create event handlers)
+        // `p` refers to the instance that becomes `this` after the super() call
+      });
+      P.p = window.p = this;
+      this.setup = this.setup.bind(this);
+      this.draw = this.draw.bind(this);
+      // this.start = this.start.bind(this);
+      // this.stop = this.stop.bind(this);
+      this.windowResized = this.windowResized.bind(this);
 
-    // store elements
-    this.el = el;
-    this.elSize = this.el.getBoundingClientRect();
+      // store elements
+      this.el = el;
+      this.elSize = this.el.getBoundingClientRect();
+    }
+
+    setup() {
+      this.createCanvas(this.elSize.width, this.elSize.height, p5.prototype.WEBGL);
+      this.el.appendChild(this.canvas);
+      this.background(0);
+      this.setupFirstFrame();
+    }
+
+    setupFirstFrame() {
+      // override this!
+    }
+
+    draw() {
+      // override this!
+      this.background(127 + 127 * Math.sin(this.frameCount * 0.01));
+    }
+
+    windowResized() {
+      this.elSize = this.el.getBoundingClientRect();
+      this.resizeCanvas(this.elSize.width, this.elSize.height);
+    }
+
+    // addFrameListener(fn) {
+    //   this.app.ticker.add(fn);
+    // }
+
+    width() {
+      return this.elSize.width;
+    }
+
+    height() {
+      return this.elSize.height;
+    }
+
   }
 
-  setup() {
-    this.createCanvas(this.elSize.width, this.elSize.height, p5.prototype.WEBGL);
-    this.el.appendChild(this.canvas);
-    this.background(0);
-    this.setupFirstFrame();
-  }
-
-  setupFirstFrame() {
-    // override this!
-  }
-
-  draw() {
-    // override this!
-    this.background(127 + 127 * Math.sin(this.frameCount * 0.01));
-  }
-
-  windowResized() {
-    this.elSize = this.el.getBoundingClientRect();
-    this.resizeCanvas(this.elSize.width, this.elSize.height);
-  }
-
-  // addFrameListener(fn) {
-  //   this.app.ticker.add(fn);
-  // }
-
-  width() {
-    return this.elSize.width;
-  }
-
-  height() {
-    return this.elSize.height;
-  }
-
-}
-
-P5Sketch.defaultVertShader = `
+  P5Sketch.defaultVertShader = `
   // vertex shader
   attribute vec3 aPosition;
   attribute vec2 aTexCoord;
@@ -2309,7 +2311,10 @@ P5Sketch.defaultVertShader = `
     // positionVec4.xy = positionVec4.xy - 0.5;
     gl_Position = positionVec4;
   }
-`;
+  `;
+
+  window.P5Sketch = P5Sketch;
+}
 
 class PageVisibility {
 
--- src/p5-sketch.es6.js
+++ src/p5-sketch.es6.js
@@ -1,63 +1,65 @@
-class P {}
-P.p = null;
-window.p = null;
+if(window.p5) {
+  class P {}
+  P.p = null;
+  window.P = P;
+  window.p = null;
 
-class P5Sketch extends p5 {
+  class P5Sketch extends p5 {
 
-  constructor(el=document.body, id='p5') {
-    super(p => {
-      // do any setup in here that needs to happen before the sketch starts
-      // (e.g. create event handlers)
-      // `p` refers to the instance that becomes `this` after the super() call
-    });
-    P.p = window.p = this;
-    this.setup = this.setup.bind(this);
-    this.draw = this.draw.bind(this);
-    // this.start = this.start.bind(this);
-    // this.stop = this.stop.bind(this);
-    this.windowResized = this.windowResized.bind(this);
+    constructor(el=document.body, id='p5') {
+      super(p => {
+        // do any setup in here that needs to happen before the sketch starts
+        // (e.g. create event handlers)
+        // `p` refers to the instance that becomes `this` after the super() call
+      });
+      P.p = window.p = this;
+      this.setup = this.setup.bind(this);
+      this.draw = this.draw.bind(this);
+      // this.start = this.start.bind(this);
+      // this.stop = this.stop.bind(this);
+      this.windowResized = this.windowResized.bind(this);
 
-    // store elements
-    this.el = el;
-    this.elSize = this.el.getBoundingClientRect();
+      // store elements
+      this.el = el;
+      this.elSize = this.el.getBoundingClientRect();
+    }
+
+    setup() {
+      this.createCanvas(this.elSize.width, this.elSize.height, p5.prototype.WEBGL);
+      this.el.appendChild(this.canvas);
+      this.background(0);
+      this.setupFirstFrame();
+    }
+
+    setupFirstFrame() {
+      // override this!
+    }
+
+    draw() {
+      // override this!
+      this.background(127 + 127 * Math.sin(this.frameCount * 0.01));
+    }
+
+    windowResized() {
+      this.elSize = this.el.getBoundingClientRect();
+      this.resizeCanvas(this.elSize.width, this.elSize.height);
+    }
+
+    // addFrameListener(fn) {
+    //   this.app.ticker.add(fn);
+    // }
+
+    width() {
+      return this.elSize.width;
+    }
+
+    height() {
+      return this.elSize.height;
+    }
+
   }
 
-  setup() {
-    this.createCanvas(this.elSize.width, this.elSize.height, p5.prototype.WEBGL);
-    this.el.appendChild(this.canvas);
-    this.background(0);
-    this.setupFirstFrame();
-  }
-
-  setupFirstFrame() {
-    // override this!
-  }
-
-  draw() {
-    // override this!
-    this.background(127 + 127 * Math.sin(this.frameCount * 0.01));
-  }
-
-  windowResized() {
-    this.elSize = this.el.getBoundingClientRect();
-    this.resizeCanvas(this.elSize.width, this.elSize.height);
-  }
-
-  // addFrameListener(fn) {
-  //   this.app.ticker.add(fn);
-  // }
-
-  width() {
-    return this.elSize.width;
-  }
-
-  height() {
-    return this.elSize.height;
-  }
-
-}
-
-P5Sketch.defaultVertShader = `
+  P5Sketch.defaultVertShader = `
   // vertex shader
   attribute vec3 aPosition;
   attribute vec2 aTexCoord;
@@ -69,4 +71,7 @@ P5Sketch.defaultVertShader = `
     // positionVec4.xy = positionVec4.xy - 0.5;
     gl_Position = positionVec4;
   }
-`;
+  `;
+
+  window.P5Sketch = P5Sketch;
+}
