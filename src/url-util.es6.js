f09f2535abfc1276ad2186da07cf1b7b7f8dcbeb
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-08-27 13:36:13 -0600

    Create url-util.es6.js

---
+++ src/url-util.es6.js
@@ -0,0 +1,15 @@
+class URLUtil {
+
+  static getHashQueryVariable(variable) {
+    var query = window.location.hash.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+        return decodeURIComponent(pair[1]);
+      }
+    }
+    return null;
+  }
+
+}
