54000768f7e5f6dacb9ddcc06ab3302a4aeebb38
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-03-23 17:00:00 -0600

    Fix/enhance GATracking object

--- src/ga-tracking.es6.js
+++ src/ga-tracking.es6.js
@@ -1,21 +1,23 @@
 class GATracking {
 
-  constructor(gaID=null) {
+  constructor(gaID=null, debug=false) {
     if(gaID != null) {
-      if(gaId.indexOf('UA-') != -1) console.warn('Please only use the numeric GA tracking id');
+      if(gaID.indexOf('UA-') != -1) console.warn('Please only use the numeric GA tracking id');
+      let debugScript = (debug) ? '_debug' : '';
       // https://developers.google.com/analytics/devguides/collection/analyticsjs/
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      })(window,document,'script','https://www.google-analytics.com/analytics'+debugScript+'.js','ga');
+      if(debug) window.ga_debug = {trace: true};
       ga('create', `UA-${gaID}`, 'auto');
       ga('send', 'pageview');
     }
   }
 
-  event(category='test', action='click') {
+  event(category='test', action='click', label=null, value=null) {
     // More info: https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-    window.ga('send', 'event', category, action);
+    window.ga('send', 'event', category, action, label, value);
   }
 
   page(path=document.location.pathname) {
