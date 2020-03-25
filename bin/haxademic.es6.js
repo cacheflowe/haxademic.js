39dcc04dd76b3d522020584f7f85077d9e12abcb
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-03-24 18:29:41 -0600

    Build compiled library

--- bin/haxademic.es6.js
+++ bin/haxademic.es6.js
@@ -1294,22 +1294,24 @@ class FontUtil {
 
 class GATracking {
 
-  constructor(gaID=null) {
+  constructor(gaID=null, debug=false) {
     if(gaID != null) {
-      if(gaId.indexOf('UA-') != -1) console.warn('Please only use the numeric GA tracking id');
+      if(gaID.indexOf('UA-') != -1) console.warn('Please only use the numeric GA tracking id');
+      let debugScript = (debug) ? '_debug' : '';
       // https://developers.google.com/analytics/devguides/collection/analyticsjs/
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      })(window,document,'script','https://www.google-analytics.com/analytics'+debugScript+'.js','ga');
+      if(debug) window.ga_debug = {trace: true};
       ga('create', `UA-${gaID}`, 'auto');
       ga('send', 'pageview');
     }
   }
 
-  event(category='test', action='click') {
+  event(category='test', action='click', label=null, value=null) {
     // More info: https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-    window.ga('send', 'event', category, action);
+    window.ga('send', 'event', category, action, label, value);
   }
 
   page(path=document.location.pathname) {
@@ -3624,10 +3626,11 @@ SVGUtil.testSVG2 = '<svg xmlns="http://www.w3.org/2000/svg" height="100" width="
 
 class ThreeScene {
 
-  constructor(el=document.body, bgColor=0xffffff) {
+  constructor(el=document.body, bgColor=0xffffff, transparent=false) {
     this.el = el;
     this.elSize = this.el.getBoundingClientRect();
     this.bgColor = bgColor;
+    this.transparent = transparent;
     this.devicePixelRatio = window.devicePixelRatio || 1;
     this.buildScene();
     this.buildRenderer();
@@ -3648,8 +3651,10 @@ class ThreeScene {
   }
 
   buildRenderer() {
-    this.renderer = new THREE.WebGLRenderer({antialias:true});
-    this.renderer.setClearColor(this.bgColor);
+    let options = {antialias: true};
+    if(this.transparent) options.alpha = true;
+    this.renderer = new THREE.WebGLRenderer(options);
+    this.renderer.setClearColor(this.bgColor, (this.transparent) ? 0 : 1);
 		this.renderer.setPixelRatio(this.devicePixelRatio);
     this.renderer.setSize(this.elSize.width, this.elSize.height);
     this.renderer.shadowMap.enabled = true;
@@ -3685,15 +3690,230 @@ class ThreeScene {
   }
 
   resize() {
+    this.fitToHtmlContainer();
+    this.camera.aspect = this.ASPECT;
+    this.camera.updateProjectionMatrix();
+  }
+
+  fitToHtmlContainer() {
     this.getContainerSize();
     this.container.style.width = this.elSize.width + 'px';
     this.container.style.height = this.elSize.height + 'px';
-    this.camera.aspect = this.ASPECT;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(this.elSize.width, this.elSize.height);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
 }
+
+class ThreeSceneAr extends ThreeScene {
+
+  // TODO:
+  // - Add a way to show a link to the jpg marker file for testing/printing
+  // - Extend THREEx.ArSmoothedControls controls to lerp scale down to hide
+  // - Instructions if someone declines camera permission
+  //   - Instructions behind camera permission popup?
+  // - [Nice-to-have] Whats up with the aspect ratio bug?? Only does it on static test
+  //   - Source image processing gets stretched in portrait?! If debug is turned on, you can see it happen
+
+  constructor(el=document.body, arJsOptions, debug=false) {
+    super(el, 0x000000, true);
+    // store props
+    this.cameraSource = arJsOptions.cameraSource;
+    this.arCameraData = arJsOptions.arCameraData;
+    this.arMarkerPatt = arJsOptions.arMarkerPatt;
+    this.markerActiveCallback = arJsOptions.markerActiveCallback || null;
+    this.debug = debug;
+    // set state
+    this.active = false;
+
+    // add ar.js on top of ThreeScene
+    this.buildArCameraSource();
+    this.buildArCV();
+    this.buildMarkerConfig();
+    this.buildSmoothedArControls();
+
+    // add shadow & lighting
+    this.buildShadowPlane();
+    this.buildLights();
+    if(this.debug) this.addTestCube();
+  }
+
+  buildArCameraSource() {
+    // set ARcamera source - either webcam or URL to static test image
+    let sourceOptions = (this.cameraSource == ThreeSceneAr.WEBCAM) ?
+      {sourceType : 'webcam'} :
+      {sourceType : 'image', sourceUrl: this.cameraSource};
+    this.arToolkitSource = new THREEx.ArToolkitSource(sourceOptions);
+
+    // init source listener
+    this.arToolkitSource.init(() => {
+      if(this.debug) console.log("AR.js ready!");
+      this.resize();
+      setTimeout(() => { window.dispatchEvent(new Event('resize')); }, 200);
+    });
+  }
+
+  buildArCV() {
+    // configure ARToolkit & image analysis params
+    this.arToolkitContext = new THREEx.ArToolkitContext({
+      debug: this.debug,
+      cameraParametersUrl: this.arCameraData,
+      detectionMode: 'mono',
+      maxDetectionRate: 30,   // max fps of CV updates
+      // canvasWidth: 80*3,		// scaleing down makes analysis faster, but also worse at marker detection
+      // canvasHeight: 60*3,
+    });
+
+    // Call init & copy projection matrix to camera when complete
+    this.arToolkitContext.init(() => {
+      this.camera.projectionMatrix.copy( this.arToolkitContext.getProjectionMatrix() );
+    });
+  }
+
+  buildMarkerConfig() {
+    // Add marker container.
+    // Don't add to actual scene, because we're using a smoothed controls container that will lerp towards `markerRoot`
+    this.markerRoot = new THREE.Group();
+    this.markerControls1 = new THREEx.ArMarkerControls(this.arToolkitContext, this.markerRoot, {
+      type : 'pattern',
+      patternUrl : this.arMarkerPatt,
+    });
+    // this.scene.add(this.markerRoot);
+  }
+
+  buildSmoothedArControls() {
+    // interpolates from last position to create smoother transitions when moving.
+    // parameter lerp values near 0 are slow, near 1 are fast (instantaneous).
+    this.arRoot = new THREE.Group();
+    this.scene.add(this.arRoot);
+    this.arSmoothControls = new THREEx.ArSmoothedControls(this.arRoot, {
+      lerpPosition: 0.3,
+      lerpQuaternion: 0.3,
+      lerpScale: 0.3,
+      minVisibleDelay: 0.2,
+      minUnvisibleDelay: 0.5,
+    });
+    this.arSmoothControls.addEventListener('becameVisible', () => {
+      this.active = true;
+      if(this.markerActiveCallback) this.markerActiveCallback(this.active);
+      if(this.debug) console.log('becameVisible event notified')
+    })
+    this.arSmoothControls.addEventListener('becameUnVisible', () => {
+      this.active = false;
+      if(this.markerActiveCallback) this.markerActiveCallback(this.active);
+      if(this.debug) console.log('becameUnVisible event notified')
+    })
+  }
+
+  buildScene() {
+    // override ThreeScene parent camera configuration
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.Camera();
+    this.scene.add(this.camera);
+  }
+
+  buildLights() {
+    // ambient light
+    let ambientLight = new THREE.AmbientLight( 0x888888, 0.5 );
+    this.scene.add(ambientLight);
+
+    // hemisphere light
+    const skyColor = 0x999999;
+    const groundColor = 0xffffff;
+    const intensity = 0.5;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+    this.scene.add(light);
+
+    // directional light
+    const color = 0xFFFFFF;
+    const dirIntensity = 0.5;
+    const lightDir = new THREE.DirectionalLight(color, dirIntensity);
+    lightDir.position.set(5, 20, 2);
+    this.scene.add(lightDir);
+  }
+
+  buildShadowPlane() {
+    // add shadow plane
+    var planeSize = 15; // 1 is the size of the AR marker
+    this.shadowPlane = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(planeSize, planeSize),
+      new THREE.ShadowMaterial({ opacity: 0.5 })
+    );
+    this.shadowPlane.rotation.x = -Math.PI/2;
+    this.shadowPlane.receiveShadow = true;
+    this.shadowPlane.depthWrite = false;
+    this.shadowPlane.position.set(0, -0.05, 0); // set the shadow plane just under the model to prevent z-clipping
+    this.arRoot.add(this.shadowPlane);
+
+    // add shadow spotlight
+    this.spotlight = new THREE.SpotLight(0xffffff);
+    this.spotlight.position.set(1, 6, 0);   // light is above and just off to a side
+    this.spotlight.target = this.shadowPlane;
+    this.spotlight.castShadow = true;
+    this.spotlight.shadow.mapSize.width = 1024;
+    this.spotlight.shadow.mapSize.height = 1024;
+    // this.spotlight.shadow.camera.near = 500;
+    // this.spotlight.shadow.camera.far = 4000;
+    // this.spotlight.shadow.camera.fov = 30;
+    // this.spotlight.shadow.radius = 80;
+    this.spotlight.penumbra = 0.01;
+    this.spotlight.decay = 1;
+    this.spotlight.angle = 0.5;
+    this.spotlight.distance = 7;
+    this.arRoot.add(this.spotlight);
+
+    // add light helper
+    var spotlightDebug = false;
+    if(spotlightDebug == true) {
+      var lightHelper = new THREE.SpotLightHelper( this.spotlight );
+      this.arRoot.add(this.lightHelper);
+    }
+  }
+
+  getScene() {
+    return this.scene;
+  }
+
+  getArRoot() {
+    return this.arRoot;
+  }
+
+  render() {
+    // update smoothed AR controls
+    this.arSmoothControls.update(this.markerRoot);
+
+    // update artoolkit on every frame
+    if(this.arToolkitSource.ready !== false ) {
+      this.arToolkitContext.update( this.arToolkitSource.domElement );
+    }
+
+    // three.js render step
+    super.render();
+  }
+
+  resize() {
+    // don't call super, but make sure we're fit to the html container
+    this.fitToHtmlContainer();
+    // update AR.js
+    this.arToolkitSource.onResizeElement();
+    this.arToolkitSource.copyElementSizeTo(this.renderer.domElement);
+    if( this.arToolkitContext.arController !== null ) {
+      this.arToolkitSource.copyElementSizeTo(this.arToolkitContext.arController.canvas);
+    }
+  }
+
+  addTestCube() {
+    // add a transparent unit cube so we can tell if things are working
+    let cubeMesh = new THREE.Mesh(
+      new THREE.CubeGeometry(1, 1, 1),
+      new THREE.MeshNormalMaterial({ transparent: true, opacity: 0.5, side: THREE.DoubleSide })
+    );
+    cubeMesh.position.y = 0.5;
+    this.arRoot.add(cubeMesh);
+  }
+
+}
+
+ThreeSceneAr.WEBCAM = "webcam";
 
 class URLUtil {
 
