11353addd1649d37cafb6a337ad0e89872dd361b
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-04-24 13:38:15 -0600

    Add basic WebRTC ScreenCapture class & demo

---
+++ demo/demo-screen-capture.es6.js
@@ -0,0 +1,25 @@
+class ScreenCaptureDemo extends DemoBase {
+
+  constructor(parentEl) {
+    super(parentEl, [
+      "../src/screen-capture.es6.js",
+    ], 'ScreenCapture', 'screen-cap-container');
+  }
+
+  init() {
+    // setup
+    this.screenCapContainer = document.getElementById('screen-cap-container');
+
+    // init ScreenCapture
+    this.ScreenCapture = new ScreenCapture((videoEl) => {
+      // attach to DOM, fit to container
+      videoEl.style.width = '100%';
+      this.screenCapContainer.appendChild(videoEl);
+    }, (error) => {
+      this.screenCapContainer.innerHTML = '[ScreenCapture ERROR] :: ' + error;
+    });
+  }
+
+}
+
+if(window.autoInitDemo) new ScreenCaptureDemo(document.body);
---
+++ src/screen-capture.es6.js
@@ -0,0 +1,21 @@
+class ScreenCapture {
+
+  constructor(callback, errorCallback) {
+    // more info & config: https://webrtc.org/getting-started/media-capture-and-constraints#media_devices
+    navigator.mediaDevices.getDisplayMedia({'video': {'cursor':'always', displaySurface:'window'}})
+      .then(stream => {
+        // add video stream to video element and return it
+        let videoEl = document.createElement('video');
+        videoEl.setAttribute('autoplay', true);
+        videoEl.setAttribute('playsinline', true);
+        videoEl.srcObject = stream;
+        callback(videoEl);
+      })
+      .catch(error => {
+        if(errorCallback) errorCallback(error);
+        else console.error('Error accessing display devices.', error);
+      }
+    );
+  }
+
+}
