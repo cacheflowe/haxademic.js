eecc79b225f9c6c2ede2cea12203c02a34e45742
cacheflowe <cacheflowe@cacheflowe.com>
2021-03-05 12:25:27 -0500

    Update demo-date-util.es6.js

--- demo/demo-date-util.es6.js
+++ demo/demo-date-util.es6.js
@@ -1,5 +1,6 @@
 import DemoBase from './demo--base.es6.js';
 import DateUtil from '../src/date-util.es6.js';
+import FrameLoop from '../src/frame-loop.es6.js';
 
 class DateUtilDemo extends DemoBase {
 
@@ -8,8 +9,15 @@ class DateUtilDemo extends DemoBase {
   }
 
   init() {
+    // animate loop
+    window.frameLoop = new FrameLoop();
+    window.frameLoop.addListener(this);
+  }
+
+  frameLoop(frameCount) {
     this.el.innerHTML = `
       <div><code>getMillis</code> = ${DateUtil.getMillis()}</div>
+      <div><code>getMillisFine</code> = ${DateUtil.getMillisFine()}</div>
       <div><code>getDateTimeStamp</code> = ${DateUtil.getDateTimeStamp()}</div>
       <div><code>getClockTimeStamp</code> = ${DateUtil.getClockTimeStamp()}</div>
       <div><code>getTimeStamp</code> = ${DateUtil.getTimeStamp()}</div>
