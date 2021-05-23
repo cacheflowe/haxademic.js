c83cd62a50dad4f2713b037f95b116a1106d9997
cacheflowe <cacheflowe@cacheflowe.com>
2021-05-23 00:12:18 -0400

    Add DomUtil.recurseDisableElements()

--- src/dom-util.es6.js
+++ src/dom-util.es6.js
@@ -73,6 +73,24 @@ class DOMUtil {
     return styleEl;
   }
 
+  static recurseDisableElements(elem, disabledElements) {
+    if( elem ) {
+      // disable clicking/dragging on selected element types
+      if( elem.tagName && disabledElements.indexOf( elem.tagName.toLowerCase() ) != -1 ) {  //  console.log('disabling: = '+elem.tagName.toLowerCase());
+        try {
+          elem.onmousedown = function(e){ return false; };  // TODO: remove this if touch events, so we can click inside??
+          elem.onselectstart = function(){ return false; };
+        } catch(err) {}
+      }
+      // loop through children and do the same
+      if( elem.childNodes.length > 0 ){
+        for( var i=0; i < elem.childNodes.length; i++ ) {
+          DOMUtil.recurseDisableElements( elem.childNodes[i], disabledElements );
+        }
+      }
+    }
+  }
+
 }
 
 export default DOMUtil;
