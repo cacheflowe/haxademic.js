c955200d8f01fb78680c2df73c23896bdfeb85ff
cacheflowe <cacheflowe@cacheflowe.com>
2021-09-25 18:29:54 -0400

    Create p5-recorder.es6.js

---
+++ src/p5-recorder.es6.js
@@ -0,0 +1,44 @@
+import VideoRecorder from 'https://cacheflowe.github.io/haxademic.js/src/video-recorder.es6.js';
+
+class p5Recorder {
+  constructor(loopFrames) {
+    // grab main p5js canvas
+    this.recordEl = select('canvas').elt;
+    // set up options for VideoRecorder object
+    const optionsOverride = {
+      fileType: 'webm',
+      audioKBPS: 320,
+      videoMBPS: 20,
+      callback: (aLink) => {
+        aLink.setAttribute('class', 'button');
+        this.recordEl.parentNode.appendChild(aLink);
+      },
+    };
+    this.videoRecorder = new VideoRecorder(this.recordEl, optionsOverride);
+
+    // frame recording config
+    this.loopFrames = loopFrames;
+    this.startFrame = loopFrames + 1;
+    this.endFrame = loopFrames * 2 + 1;
+    this.numFramesRecorded = 0;
+  }
+
+  renderVideo() {
+    if(frameCount == this.startFrame) {
+      this.videoRecorder.start();
+      console.log('p5Recorder :: start frame :: ', frameCount);
+    }
+    if(this.videoRecorder.recording()) {
+      this.numFramesRecorded++;
+      this.videoRecorder.addFrame(); // record frames!
+    }
+    if(frameCount == this.endFrame) {
+      this.videoRecorder.finish();
+      console.log('p5Recorder :: finish frame ::', frameCount);
+      console.log('p5Recorder :: total frames recorded:: ', this.numFramesRecorded);
+    }
+  }
+}
+
+// set class on window since this gets imported into editor.p5js.org, not as a module
+window.p5Recorder = p5Recorder;
