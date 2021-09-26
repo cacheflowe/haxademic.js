068e96f66ccef1895c2ab25846b910a37a1df996
cacheflowe <cacheflowe@cacheflowe.com>
2021-09-25 20:13:12 -0400

    Update p5-recorder.es6.js

--- src/p5-recorder.es6.js
+++ src/p5-recorder.es6.js
@@ -11,6 +11,7 @@ class p5Recorder {
       videoMBPS: 20,
       callback: (aLink) => {
         aLink.setAttribute('class', 'button');
+        aLink.setAttribute('style', "padding: 1rem; display: inline-block; background: black; color: white; margin-top: 0.5rem; text-decoration: none; font-family: Helvetica, Arial, sans-serif; border-radius: 0.5rem;");
         this.recordEl.parentNode.appendChild(aLink);
       },
     };
@@ -42,3 +43,36 @@ class p5Recorder {
 
 // set class on window since this gets imported into editor.p5js.org, not as a module
 window.p5Recorder = p5Recorder;
+
+
+// add to the top of your sketch
+/*
+
+////////////////////////////////////////////////
+// Editable properties:
+// Edit `loopFrames` to change the loop duration
+// And disable `saveVideo` while you're working on your sketch
+var loopFrames = 240; // 4-second loop (60fps * 4)
+let saveVideo = true;
+
+// Loop properties that help you loop elements in your animation.
+// These are updated in `updateLoopRecording()`
+let frameCountLooped = 1;
+let loopProgress = 0;
+let loopProgressRadians = 0;
+
+// call this function at the end of `draw()`
+var recorder = null;
+function updateLoopRecording() {
+  if(!recorder) recorder = new p5Recorder(loopFrames);
+  // create a looped framecount & normalized progress
+  frameCountLooped = frameCount % loopFrames;
+  loopProgress = frameCountLooped / loopFrames;
+  loopProgressRadians = loopProgress * TWO_PI;
+  // update video recorder
+  if(!!recorder) recorder.renderVideo();
+}
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+*/
