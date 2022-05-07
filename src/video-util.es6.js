7573283ae5b99bde55f45b957f2bd2645b4dca8c
cacheflowe <cacheflowe@cacheflowe.com>
2022-05-07 12:16:16 -0400

    Create video-util.es6.js

---
+++ src/video-util.es6.js
@@ -0,0 +1,52 @@
+class VideoUtil {
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+
+  // getters
+
+  static curTime(videoEl) {
+    return (!VideoUtil.durationValid(videoEl)) ? 0 : videoEl.currentTime;
+  }
+  
+  static curTimeNorm(videoEl) {
+    return (!VideoUtil.durationValid(videoEl)) ? 0 : videoEl.currentTime / videoEl.duration;
+  }
+
+  static duration(videoEl) {
+    return (!VideoUtil.durationValid(videoEl)) ? -1 : videoEl.duration;
+  }
+
+  static durationValid(videoEl) {
+    return (videoEl && videoEl.duration > 1);
+  }
+
+  // setters
+  
+  static setMuted(videoEl, muted) {
+    videoEl.defaultMuted = muted;
+    videoEl.muted = muted;
+  }
+  
+  static setTime(videoEl, time) {
+    videoEl.currentTime = time;
+  }
+  
+  static setTimeNorm(videoEl, timeNorm) {
+    if((!VideoUtil.durationValid(videoEl))) {
+      console.warn('VideoUtil.setTimeNorm() failed - no duration');
+      return;
+    }
+    videoEl.currentTime = timeNorm * videoEl.duration;
+  }
+
+  static setRate(videoEl, rate=1) {
+    videoEl.playbackRate = rate;
+  }
+
+  static setLoop(videoEl, loops=true) {
+    videoEl.loop = loops;
+  }
+  
+}
+
+export default VideoUtil;
