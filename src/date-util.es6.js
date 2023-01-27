20223d5f8b76d866b3f35387a3f8c91d1ce80d71
cacheflowe <cacheflowe@cacheflowe.com>
2023-01-26 22:03:07 -0500

    Update date-util.es6.js

--- src/date-util.es6.js
+++ src/date-util.es6.js
@@ -1,7 +1,6 @@
 class DateUtil {
-
   static getMillis() {
-    return (new Date()).getTime();
+    return new Date().getTime();
   }
 
   static getMillisFine() {
@@ -10,7 +9,6 @@ class DateUtil {
 
   static getDateTimeStamp() {
     const date = new Date();
-    // return (date.getYear() + 1900) + '-' + (date.getMonth() + 1) + '-' + date.getDate();
     return date.toISOString().slice(0, 10);
   }
 
@@ -20,15 +18,22 @@ class DateUtil {
   }
 
   static getTimeStamp() {
-    return DateUtil.getDateTimeStamp() + '-' + DateUtil.getClockTimeStamp();
+    return DateUtil.getDateTimeStamp() + "-" + DateUtil.getClockTimeStamp();
+  }
+
+  static getTimeStamp2() {
+    return new Date()
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", "_")
+      .replaceAll(":", "-");
   }
 
   static datesAreEqual(date1, date2) {
     return date1.getTime() == date2.getTime();
   }
-
 }
 
-DateUtil.midnightTimeSuffix = 'T00:00:00Z';
+DateUtil.midnightTimeSuffix = "T00:00:00Z";
 
 export default DateUtil;
