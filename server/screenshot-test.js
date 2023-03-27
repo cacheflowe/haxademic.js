861f2d9392277af4cd54116b152cea1edd1d0bb3
cacheflowe <cacheflowe@cacheflowe.com>
2023-03-27 10:30:22 -0400

    Update screenshot-test.js

--- server/screenshot-test.js
+++ server/screenshot-test.js
@@ -1,89 +1,126 @@
 // install:
 // - npm install screenshot-desktop
 // run:
-// - node screenshot-test.js [--debug]
+// - node screenshot-test.mjs
 
 /////////////////////
 // Imports
 /////////////////////
 
 import fs from "fs";
+import path from "path";
+import os from "os";
 import screenshot from "screenshot-desktop";
 
 /////////////////////
-// Config
+// Dashboard Config
 /////////////////////
 
-const heartbeatInterval = 60 * 1000 * 5;
-const screenshotInterval = 60 * 1000 * 15;
-const appId = "brewers-protect-the-plate";
-const appTitle = "Brewers Protect the Plate";
+const oneMinuteMS = 60 * 1000;
+const heartbeatInterval = oneMinuteMS * 5;
+const screenshotInterval = oneMinuteMS * 15;
+const appId = "example-project";
+const appTitle = "Example Project";
 const dashboardURL = "http://localhost/haxademic/www/dashboard-new/";
 
 // globals
+
 let startTime = Date.now();
-const screenshotFileName = "screenshot.jpg";
+let tmpDir = "tmp/";
+let screenshotFilePath = `screenshot.jpg`;
+
+// override with proper system temp dir
+await fs.mkdtemp(path.join(os.tmpdir(), "example-project-"), (err, folder) => {
+  if (err) throw err;
+  tmpDir = folder;
+  screenshotFilePath = `${folder}/${screenshotFilePath}`;
+});
 
 /////////////////////
 // post to dashboard
 /////////////////////
 
-function postToDashboard(imageScreenshot = null) {
-  // build post data for dashboard
+function buildPostData(screenshot = null) {
   let postData = {
     appId: appId,
     appTitle: appTitle,
-    // imageExtra: null,
     uptime: Math.round((Date.now() - startTime) / 1000),
-    // resolution: `${window.innerWidth}x${window.innerHeight}`,
     frameCount: 1,
     frameRate: 60,
+    // for front-end if we add that:
+    // resolution: `${window.innerWidth}x${window.innerHeight}`,
+    // imageExtra: null,
   };
-  if (imageScreenshot) {
-    postData.imageScreenshot = imageScreenshot;
+  if (screenshot) {
+    postData.imageScreenshot = screenshot;
   }
+  return JSON.stringify(postData);
+}
 
-  // post it!
+function postToDashboard(screenshot = null) {
   fetch(dashboardURL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(postData),
+    body: buildPostData(screenshot),
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log("Success:", data);
+      // console.log("Success:", data);
+      if (screenshot) deleteScreenshot();
     })
     .catch((error) => {
       console.error("Error:", error);
+      if (screenshot) deleteScreenshot();
     });
 }
 
 /////////////////////
-// screenshot check-in
+// Screenshot check-in
 /////////////////////
 
-setInterval(() => {
-  screenshot({ format: "jpg", filename: screenshotFileName })
+function postScreenshotCheckin() {
+  screenshot({ format: "jpg", filename: screenshotFilePath })
     .then((img) => {
-      // img: Buffer of the image - not sure what this is
-      fs.readFile(screenshotFileName, function (err, data) {
+      fs.readFile(screenshotFilePath, (err, data) => {
         let base64Img = Buffer.from(data).toString("base64");
-        // 'data:image/jpeg;base64,' + base64Img
         postToDashboard(base64Img);
       });
     })
     .catch((err) => {
       console.error("Failed to post screenshot", err);
     });
-}, screenshotInterval);
+}
+
+function deleteScreenshot() {
+  fs.unlink(screenshotFilePath, (err) => {
+    if (err) return console.error(err);
+  });
+}
+
+function listFilesInDir(dir) {
+  fs.readdir(dir, function (err, files) {
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    } else {
+      console.log("Listing files in", tmpDir, files.length);
+      if (files && files.length) {
+        files.forEach((file) => console.log("-", file));
+      } else {
+        console.log("No files found");
+      }
+    }
+  });
+}
 
 /////////////////////
-// non-screenshot heartbeat check-in
+// Set intervals
+// - Screenshot heartbeat check-in less frequently
+// - Non-screenshot heartbeat check-in more frequently
+// - Post immediately on startup
 /////////////////////
 
-setInterval(() => {
-  postToDashboard();
-}, heartbeatInterval);
+setInterval(() => postScreenshotCheckin(), screenshotInterval);
+setInterval(() => postToDashboard(), heartbeatInterval);
 postToDashboard();
