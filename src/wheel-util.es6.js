7915b5a314eefe3f5484ca0f8f9ae70e06f7b6b7
cacheflowe <cacheflowe@cacheflowe.com>
2023-04-22 10:19:46 -0400

    Update wheel-util.es6.js

--- src/wheel-util.es6.js
+++ src/wheel-util.es6.js
@@ -1,17 +1,19 @@
 class WheelUtil {
-
   constructor() {}
 
-  static addWheelListener(callback, el=document, preventDefault=false) {
-    el.addEventListener('wheel', (e) => {
-      if(preventDefault) {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-      callback(e.deltaX, e.deltaY, e);
-    }, {capture: false, passive: !preventDefault})  // if we want to cancel, it can't be passive
+  static addWheelListener(callback, el = document, preventDefault = false) {
+    el.addEventListener(
+      "wheel",
+      (e) => {
+        if (preventDefault) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+        callback(e.deltaX, e.deltaY, e);
+      },
+      { capture: false, passive: !preventDefault }
+    ); // if we want to cancel, it can't be passive
   }
-
 }
 
 export default WheelUtil;
