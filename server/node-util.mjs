cac8f17f78eef53fefb21ba9bda5af4f593dce1b
cacheflowe <cacheflowe@cacheflowe.com>
2023-04-24 18:28:48 -0400

    Create node-util.mjs

---
+++ server/node-util.mjs
@@ -0,0 +1,65 @@
+import fs from "fs";
+import path from "path";
+
+// script args ------------------------
+
+export function getScriptArgs() {
+  return process.argv.slice(2);
+}
+
+export function argExists(argKey) {
+  return args.indexOf(argKey) != -1;
+}
+
+// files util -------------------------
+
+export function fileOrDirExists(path) {
+  return fs.existsSync(path);
+}
+
+export async function createDir(dirPath) {
+  if (!fileOrDirExists(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+}
+
+export function deleteDir(dirPath) {
+  fs.rmdirSync(dirPath, { recursive: true });
+}
+
+export function filesInDir(dirPath) {
+  return fs.readdirSync(dirPath);
+}
+
+export async function fileSizeBytes(filePath) {
+  let fileStats = await fs.promises.stat(filePath);
+  return fileStats.size;
+}
+
+export async function fileSizeMb(filePath) {
+  return (await fileSizeBytes(filePath)) / (1024 * 1000);
+}
+
+export async function writeStringToFile(filePath, contentStr) {
+  return await fs.writeFileSync(filePath, contentStr);
+}
+
+// logging ----------------------------
+
+export function bigLog(msg) {
+  console.log("===================================");
+  console.log("\x1b[42m%s\x1b[0m", msg);
+  console.log("===================================");
+}
+
+export default {
+  getScriptArgs,
+  argExists,
+  fileOrDirExists,
+  createDir,
+  deleteDir,
+  filesInDir,
+  fileSizeBytes,
+  fileSizeMb,
+  bigLog,
+};
