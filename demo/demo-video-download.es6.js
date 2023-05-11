e9709987fec130f57d8417136815a78e7d827928
Justin Gitlin <cacheflowe@cacheflowe.com>
2023-05-11 09:50:56 -0400

    Update demo-video-download.es6.js

--- demo/demo-video-download.es6.js
+++ demo/demo-video-download.es6.js
@@ -14,50 +14,87 @@ class VideoDownloadDemo extends DemoBase {
   }
 
   init() {
+    this.initVideo();
+    setTimeout(() => {
+      this.initImage();
+    }, 1000);
+  }
+
+  initVideo() {
     // add video element
     let videoPath = "../data/videos/wash-your-hands.mp4";
     this.videoEl = VideoUtil.buildVideoEl(videoPath, true);
-    this.videoEl.style.setProperty("width", "100%");
-    this.debugEl.appendChild(this.videoEl);
+    this.videoEl.style.setProperty("width", "320px");
+    this.el.appendChild(this.videoEl);
 
     // add link
-    this.linkEl = document.createElement("a");
-    this.linkEl.innerText = "Download Video";
-    this.linkEl.setAttribute("href", "../data/videos/wash-your-hands.mp4");
-    this.linkEl.setAttribute("download", "wash-your-hands.mp4");
-    this.el.appendChild(this.linkEl);
+    let linkEl = document.createElement("a");
+    linkEl.innerText = "Download Video";
+    linkEl.setAttribute("href", videoPath);
+    linkEl.setAttribute("download", "wash-your-hands.mp4");
+    this.el.appendChild(linkEl);
 
-    // load video to blob
-    fetch(this.videoEl.src)
-      .then((response) => {
-        return response.blob();
-      })
-      .then((blob) => {
-        var file = new File([blob], "wash-your-hands.mp4", {
-          type: "video/mpeg",
-        });
-        var filesArray = [file];
+    linkEl.addEventListener("click", (e) => {
+      if (MobileUtil.isIOS()) {
+        e.preventDefault();
+        this.shareFile(videoPath, "video/mpeg", "wash-your-hands.mp4");
+      } else {
+        // do nothing - use default browser behavior
+      }
+    });
+  }
 
-        // click event to launch share api
-        this.linkEl.addEventListener("click", (e) => {
-          if (MobileUtil.isIOS()) {
-            // ios solution
-            // otherwise use download attribute
-            e.preventDefault();
-            if (
-              navigator.canShare &&
-              navigator.canShare({ files: filesArray })
-            ) {
-              navigator.share({
-                text: "video_file",
-                files: filesArray,
-                // title: "Your Video",
-                // url: this.videoEl.src,
-              });
-            }
-          }
-        });
-      });
+  initImage() {
+    // add video element
+    let imagePath = "../data/images/bb.jpg";
+    this.imageEl = document.createElement("img");
+    this.imageEl.setAttribute("src", imagePath);
+    this.imageEl.style.setProperty("width", "320px");
+    this.el.appendChild(this.imageEl);
+
+    // add link
+    let linkEl = document.createElement("a");
+    linkEl.innerText = "Download image";
+    linkEl.setAttribute("href", imagePath);
+    linkEl.setAttribute("download", "bb.jpg");
+    linkEl.style.setProperty("display", "block");
+
+    this.el.appendChild(linkEl);
+
+    if (MobileUtil.isIOS()) {
+      linkEl.innerText =
+        "Tap & Hold Image to Save - it's the only way to get an image to the iOS photo roll";
+    }
+
+    linkEl.addEventListener("click", (e) => {
+      if (MobileUtil.isIOS()) {
+        e.preventDefault();
+        // maybe someday this will work:
+        // this.shareFile(imagePath, "image/jpeg", "bb.jpg");
+      } else {
+        // do nothing - use default browser behavior
+      }
+    });
+  }
+
+  // if (
+  //   navigator.canShare &&
+  //   navigator.canShare({ files: filesArray })
+  // ) {
+
+  async shareFile(filePath, type = "image/jpeg", saveFile = "download.jpg") {
+    const response = await fetch(filePath);
+    const blob = await response.blob();
+    const filesArray = [
+      new File([blob], saveFile, {
+        type: type,
+        // lastModified: new Date().getTime(), // doesn't work. video file will get saved with an old date in the photo roll
+      }),
+    ];
+    const shareData = {
+      files: filesArray,
+    };
+    navigator.share(shareData);
   }
 }
 
