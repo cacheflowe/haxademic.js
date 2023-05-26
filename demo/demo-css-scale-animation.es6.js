fa62f1831d5aedc96a9f8b39556f93c95108c39f
Justin Gitlin <cacheflowe@cacheflowe.com>
2023-05-26 17:52:35 -0400

    Update demo-css-scale-animation.es6.js

--- demo/demo-css-scale-animation.es6.js
+++ demo/demo-css-scale-animation.es6.js
@@ -15,7 +15,7 @@ class DateUtilDemo extends DemoBase {
     this.injectCSS(`
       #animation-container {
         display: grid;
-        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(3, 1fr);
         column-gap: 10px;
         row-gap: 10px;
       }
@@ -24,27 +24,27 @@ class DateUtilDemo extends DemoBase {
         background-image: url("../data/images/bb.jpg");
         background-size: 100%;
         background-position: center;
-        animation: scale 4s ease-in-out infinite alternate;
+        animation: scale 6s ease-in-out infinite alternate;
       }
       @keyframes scale {
         from {
           background-size: 110%;
         }
         to {
-          background-size: 140%;
+          background-size: 120%;
         }
       }
     `);
-    this.el.innerHTML = `
-      <div style="animation-delay: ${this.randomOffset()}s;"></div>
-      <div style="animation-delay: ${this.randomOffset()}s;"></div>
-      <div style="animation-delay: ${this.randomOffset()}s;"></div>
-      <div style="animation-delay: ${this.randomOffset()}s;"></div>
-    `;
+    this.el.innerHTML = new Array(16)
+      .fill(0)
+      .map(() => {
+        return `<div style="animation-delay: ${this.randomOffset()}s;"></div>`;
+      })
+      .join("");
   }
 
   randomOffset() {
-    return Math.random() * 4;
+    return Math.random() * 6;
   }
 }
 
