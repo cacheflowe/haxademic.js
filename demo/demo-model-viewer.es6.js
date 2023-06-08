6c367c8313b6350b2a023cc4ed4afe82328a04c2
cacheflowe <cacheflowe@cacheflowe.com>
2023-06-08 10:42:57 -0400

    Fix model-viewer demo

--- demo/demo-model-viewer.es6.js
+++ demo/demo-model-viewer.es6.js
@@ -1,42 +1,56 @@
-import DemoBase from './demo--base.es6.js';
+import DemoBase from "./demo--base.es6.js";
+import DOMUtil from "../src/dom-util.es6.js";
 
 class ModelViewerDemo extends DemoBase {
-
   constructor(parentEl) {
-    super(parentEl, [
-      // "https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js",
-      "!https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js",
-    ], `
-      <div class="container">
-        <style>
-          .container {
-            background-color: gray;
-          }
-          model-viewer {
-            width: 100%;
-            height: 500px;
-            background-color: gray;
-          }
-        </style>
-        <h1>ModelViewer</h1>
-        <p>Here's a duck</p>
-        <model-viewer loading="lazy" camera-controls auto-rotate shadow-intensity="0.5" shadow-softness="0.5" --progress-bar-color="rgba(0,0,0,0)" src="../data/duck.gltf" alt="A 3D model of a duck"></model-viewer>
+    super(
+      parentEl,
+      null,
+      "ModelViewer",
+      "model-viewer-container",
+      "Click & drag to launch html particles in the box below"
+    );
+  }
+
+  init() {
+    // inject *module* js
+    DOMUtil.loadJavascript(
+      "https://ajax.googleapis.com/ajax/libs/model-viewer/3.1.1/model-viewer.min.js",
+      () => {
+        this.loadModels();
+      },
+      "module"
+    );
+  }
+
+  loadModels() {
+    this.injectCSS(`
+      .container {
+      }
+      model-viewer {
+        width: 100%;
+        height: 500px;
+        background-color: #333;
+        margin-bottom: var(--pico-typography-spacing-vertical);
+      }
+    `);
+    this.injectHTML(`
+      <div>
         <p>
           <b>Info</b>:<br>
           - https://modelviewer.dev/<br>
           - https://cwervo.com/writing/quicklook-web/<br>
           - https://developer.apple.com/news/?id=01132020a<br>
         </p>
-        <model-viewer loading="lazy" camera-controls auto-rotate shadow-intensity="0.5" shadow-softness="0.5" --progress-bar-color="rgba(0,0,0,0)" src="../data/duck.gltf" alt="A 3D model of a duck"></model-viewer>
+        <p>Here's a duck</p>
+        <model-viewer loading="lazy" camera-controls auto-rotate shadow-intensity="0.5" shadow-softness="0.5" --progress-bar-color="rgba(0,0,0,0)" src="../data/models/duck-v1/duck.gltf" alt="A 3D model of a duck"></model-viewer>
+        <p>And a fox</p>
+        <model-viewer loading="lazy" camera-controls auto-rotate shadow-intensity="0.5" shadow-softness="0.5" --progress-bar-color="rgba(0,0,0,0)" src="../data/models/fox/Fox.gltf" alt="A 3D model of a fox"></model-viewer>
         <p>One more duck</p>
-        <model-viewer loading="lazy" camera-controls auto-rotate shadow-intensity="0.5" shadow-softness="0.5" --progress-bar-color="rgba(0,0,0,0)" src="../data/duck.gltf" alt="A 3D model of a duck"></model-viewer>
+        <model-viewer loading="lazy" camera-controls auto-rotate shadow-intensity="0.5" shadow-softness="0.5" --progress-bar-color="rgba(0,0,0,0)" src="../data/models/duck/Duck.gltf" alt="A 3D model of a duck"></model-viewer>
       </div>
     `);
   }
-
-  init() {
-  }
-
 }
 
-if(window.autoInitDemo) window.demo = new ModelViewerDemo(document.body);
+if (window.autoInitDemo) window.demo = new ModelViewerDemo(document.body);
