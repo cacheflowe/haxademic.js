c30316e156ae66ab303e3c9d4c2600c3dfa705f9
cacheflowe <cacheflowe@cacheflowe.com>
2023-09-20 20:44:35 -0400

    Update app-store-debug.es6.js

--- src/app-store-debug.es6.js
+++ src/app-store-debug.es6.js
@@ -29,6 +29,7 @@ class AppStoreDebug {
       left: 0;
       padding: 12px;
       height: 100%;
+      max-width: 70%;
       overflow-y: auto;
       opacity: 0.9;
       z-index: 9999;
@@ -43,22 +44,32 @@ class AppStoreDebug {
     if (this.showing) this.printStore();
   }
 
+  isShowing() {
+    return this.showing;
+  }
+
   show() {
     this.printStore();
     this.el.style.display = "block";
+    this.showing = true;
   }
 
   hide() {
     this.el.innerHTML = "";
     this.el.style.display = "none";
+    this.showing = false;
   }
 
   printStore() {
     let htmlStr = "<table>";
     for (let storeKey in _store.state) {
       let val = _store.state[storeKey];
-      if (val && typeof val == "object" && val.length && val.length > 0)
+      if (val && typeof val == "object" && val.length && val.length > 0) {
         val = `Array(${val.length})`; // special display for arrays
+      }
+      if (val && typeof val == "string" && val.length && val.length > 100) {
+        val = `${val.substring(0, 100)}...`; // special display for long strings
+      }
       htmlStr += `<tr><td>${storeKey}</td><td>${val}</td></tr>`;
     }
     htmlStr += "</table>";
