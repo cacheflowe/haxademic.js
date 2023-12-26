e5414f867b92078c7d77792f4eebe90f21454937
cacheflowe <cacheflowe@cacheflowe.com>
2023-12-26 11:16:17 -0500

    Create crypto.js

---
+++ src/crypto.js
@@ -0,0 +1,31 @@
+class Crypto {
+  // from: https://stackoverflow.com/a/66938952
+
+  static encode(salt, text) {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = (code) =>
+      textToChars(salt).reduce((a, b) => a ^ b, code);
+
+    return text
+      .split("")
+      .map(textToChars)
+      .map(applySaltToChar)
+      .map(byteHex)
+      .join("");
+  }
+
+  static decode(salt, encoded) {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const applySaltToChar = (code) =>
+      textToChars(salt).reduce((a, b) => a ^ b, code);
+    return encoded
+      .match(/.{1,2}/g)
+      .map((hex) => parseInt(hex, 16))
+      .map(applySaltToChar)
+      .map((charCode) => String.fromCharCode(charCode))
+      .join("");
+  }
+}
+
+export default Crypto;
