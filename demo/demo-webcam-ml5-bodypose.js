71bdad6523b643eba598bf75c77682eba9bc0976
Justin Gitlin <cacheflowe@cacheflowe.com>
2024-09-07 16:39:31 -0400

    Move & improve bodypose demo w/persistent skeleton tracking

--- demo/demo-webcam-ml5-bodypose.js
+++ demo/demo-webcam-ml5-bodypose.js
@@ -1,6 +1,7 @@
 import DemoBase from "./demo--base.js";
 import DOMUtil from "../src/dom-util.js";
 import EventLog from "../src/event-log.js";
+import MathUtil from "../src/math-util.js";
 import ImageUtil from "../src/image-util.js";
 import FrameLoop from "../src/frame-loop.js";
 import Webcam from "../src/webcam.js";
@@ -11,9 +12,10 @@ import Stats from "../vendor/stats.module.js";
 // - https://unpkg.com/ml5@1.0.1/dist/ml5.js
 //   - Came from: https://unpkg.com/ml5@latest/dist/ml5.js
 // - Original demo code: https://editor.p5js.org/ml5/sketches
+// - https://editor.p5js.org/ml5/sketches/hMN9GdrO3
 
 // NOTES
-// - https://editor.p5js.org/ml5/sketches/c8sl_hGmN
+// - https://editor.p5js.org/ml5/sketches/c8sl_hGmN (OLD)
 // - https://github.com/ml5js/ml5-next-gen/blob/main/src/Bodypose/index.js
 // - https://github.com/ml5js/ml5-library/issues/478 - loading models offline
 // - https://www.kaggle.com/models/google/movenet
@@ -24,8 +26,9 @@ import Stats from "../vendor/stats.module.js";
 // TODO
 // - Try shrinking the image into a <canvas> to speed up detection
 //   - Why is it so much slower with the webcam?
-// - Track skeletons over time by checking distance between frames and matching them up in a Map
-//   - Interpolate the joints for smoothness
+// - [WORKING] Track skeletons over time by checking distance between frames and matching them up in a Map
+//   - Hang onto skeletons for a few frames before pruning them, since a pose might be lost for several frames. Have a secondary array of skeletons that are "old", to make sure we check/match recent skeletons first
+//   - Ignore arms/hads/elbows, since these can flail, and increase the threshold for matching skeletons
 
 class MediapipeBodyTrackingDemo extends DemoBase {
   constructor(parentEl) {
@@ -33,8 +36,8 @@ class MediapipeBodyTrackingDemo extends DemoBase {
       parentEl,
       // ["../vendor/ml5-v0.20.0-alpha.4.js"],
       ["../vendor/ml5.js"],
-      "ml5 BodyPose MoveNet Demo",
-      "ml5-bodypose-movenet-demo",
+      "ml5 BodyPose Demo",
+      "ml5-bodypose-demo",
       `From the <a href="https://editor.p5js.org/ml5/sketches/c8sl_hGmN">ml5 bodypose/movenet</a> demo for multiple pose detection.`
     );
   }
@@ -143,11 +146,14 @@ class MediapipeBodyTrackingDemo extends DemoBase {
       );
     }, 1000);
     this.poses = [];
+    this.skeletons = [];
     this.connections = this.bodyPose.getSkeleton(); // same array as Joints.pairs
   }
 
   gotPoses(results) {
     this.poses = results;
+    this.posesCount = results.length;
+    this.comparePoses();
   }
 
   attachDetectionToVideo() {
@@ -187,6 +193,62 @@ class MediapipeBodyTrackingDemo extends DemoBase {
     this.canvasElement.height = videoRect.height;
   }
 
+  comparePoses() {
+    if (!this.poses) return;
+    // reset for pruning
+    for (let j = 0; j < this.skeletons.length; j++) {
+      let skeleton = this.skeletons[j];
+      skeleton.reset();
+    }
+
+    // step 1: loop through poses and skeletons backwards, and update existing skeletons if they are close enough in total position. remove poses that have been matched
+    // step 2: for remaining poses with no match, create a new skeleton and pass in pose data, and remove pose from list
+    for (let i = this.poses.length - 1; i >= 0; i--) {
+      let pose = this.poses[i];
+      let foundMatch = false;
+      for (let j = 0; j < this.skeletons.length; j++) {
+        if (foundMatch == false) {
+          let skeleton = this.skeletons[j];
+          let dist = this.distanceBetweenSkeletons(pose, skeleton);
+          // console.log(dist);
+          if (dist < 1500) {
+            foundMatch = true;
+            skeleton.update(pose);
+            this.poses.splice(i, 1);
+          }
+        }
+      }
+      if (!foundMatch) {
+        this.skeletons.push(new Skeleton(pose));
+        this.poses.splice(i, 1);
+      }
+    }
+
+    // prune old skeletons
+    for (let j = this.skeletons.length - 1; j >= 0; j--) {
+      let skeleton = this.skeletons[j];
+      if (skeleton.didUpdate() == false) {
+        this.skeletons.splice(j, 1);
+      }
+    }
+  }
+
+  distanceBetweenSkeletons(pose, skeleton) {
+    let totalDist = 0;
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let posePoint = pose.keypoints[i];
+      let skeletonPoint = skeleton.joints()[i];
+      let dist = MathUtil.getDistance(
+        posePoint.x,
+        posePoint.y,
+        skeletonPoint.x,
+        skeletonPoint.y
+      );
+      totalDist += dist;
+    }
+    return totalDist;
+  }
+
   handleSkeletons() {
     if (!this.canvasElement) return;
     if (!this.poses) return;
@@ -199,32 +261,39 @@ class MediapipeBodyTrackingDemo extends DemoBase {
     this.canvasCtx.clearRect(0, 0, canvW, canvH);
 
     // debug
-    this.debugEl.innerHTML = `
-      <div>poses.length: ${this.poses.length}</div>
+    if (this.posesCount) {
+      this.debugEl.innerHTML = `
+      <div>poses.length: ${this.posesCount}</div>
+      <div>skeletons.length: ${this.skeletons.length}</div>
+      <div>Skeleton.count: ${Skeleton.count}</div>
     `;
+    }
 
     // Draw all the tracked landmark points
-    for (let i = 0; i < this.poses.length; i++) {
-      let pose = this.poses[i];
-      this.drawSkeleton(pose);
+    for (let i = 0; i < this.skeletons.length; i++) {
+      this.drawSkeleton(this.skeletons[i]);
     }
 
     // finish drawing
     this.canvasCtx.restore();
   }
 
-  drawSkeleton(pose) {
+  drawSkeleton(skeleton, index) {
     // canvas-scaling helpers
     let scale = this.scale();
 
+    // color per skeleton
+    let curColor = skeleton.debugColor;
+
     // console.log(pose.keypoints.length); // 17!
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      let keypoint = pose.keypoints[j];
+    let keypoints = skeleton.joints();
+    for (let j = 0; j < keypoints.length; j++) {
+      let keypoint = keypoints[j];
       // console.log(keypoint);
       // Only draw a circle if the keypoint's confidence is bigger than 0.1
       if (keypoint.confidence > 0.1) {
-        this.canvasCtx.fillStyle = "#ffffff";
-        this.canvasCtx.fillRect(
+        this.canvasCtx.strokeStyle = curColor;
+        this.canvasCtx.strokeRect(
           keypoint.x * scale - 5,
           keypoint.y * scale - 5,
           10,
@@ -236,23 +305,29 @@ class MediapipeBodyTrackingDemo extends DemoBase {
     // draw lines between pairs
     for (let j = 0; j < Joints.pairs.length; j++) {
       let pair = Joints.pairs[j];
-      let a = pose.keypoints[pair[0]];
-      let b = pose.keypoints[pair[1]];
+      let a = keypoints[pair[0]];
+      let b = keypoints[pair[1]];
       this.canvasCtx.beginPath();
       this.canvasCtx.moveTo(a.x * scale, a.y * scale);
       this.canvasCtx.lineTo(b.x * scale, b.y * scale);
-      this.canvasCtx.strokeStyle = "#ffffff";
+      this.canvasCtx.strokeStyle = curColor;
       this.canvasCtx.strokeWeight = 3;
       this.canvasCtx.stroke();
     }
 
     // draw image
     let torsoTopX =
-      (this.x(pose, Joints.shoulderL) + this.x(pose, Joints.shoulderR)) / 2;
+      (this.x(keypoints, Joints.shoulderL) +
+        this.x(keypoints, Joints.shoulderR)) /
+      2;
     let torsoTopY =
-      (this.y(pose, Joints.shoulderL) + this.y(pose, Joints.shoulderR)) / 2;
-    let torsoBotX = (this.x(pose, Joints.hipL) + this.x(pose, Joints.hipR)) / 2;
-    let torsoBotY = (this.y(pose, Joints.hipL) + this.y(pose, Joints.hipR)) / 2;
+      (this.y(keypoints, Joints.shoulderL) +
+        this.y(keypoints, Joints.shoulderR)) /
+      2;
+    let torsoBotX =
+      (this.x(keypoints, Joints.hipL) + this.x(keypoints, Joints.hipR)) / 2;
+    let torsoBotY =
+      (this.y(keypoints, Joints.hipL) + this.y(keypoints, Joints.hipR)) / 2;
     let torsoCenterX = (torsoTopX + torsoBotX) / 2;
     let torsoCenterY = (torsoTopY + torsoBotY) / 2;
     let torsoH = Math.sqrt(
@@ -281,19 +356,18 @@ class MediapipeBodyTrackingDemo extends DemoBase {
     return this.canvasElement.width / this.videoEl.videoWidth;
   }
 
-  x(pose, joint) {
-    return pose.keypoints[joint].x * this.scale();
+  x(keypoints, joint) {
+    return keypoints[joint].x * this.scale();
   }
 
-  y(pose, joint) {
-    return pose.keypoints[joint].y * this.scale();
+  y(keypoints, joint) {
+    return keypoints[joint].y * this.scale();
   }
 
   keyDown(key) {}
 
   frameLoop(frameCount) {
     if (this.stats) this.stats.begin();
-    // this.predictWebcam();
     this.handleSkeletons();
     if (this.stats) this.stats.end();
   }
@@ -337,34 +411,57 @@ class Joints {
     [Joints.kneeL, Joints.ankleL],
     [Joints.kneeR, Joints.ankleR],
   ];
+
+  static colors = [
+    "#ffff00",
+    "#ff00ff",
+    "#00ffff",
+    "#0000ff",
+    "#ff0000",
+    "#0000ff",
+  ];
 }
 
-/*
 class Skeleton {
-  constructor(landmarks) {
-    this.landmarks = landmarks;
+  constructor(pose) {
+    this.pose = pose;
     this.lastUpdate = performance.now();
+    this.updatedLastFrame = true;
+    Skeleton.count++;
+    this.debugColor = Joints.colors[Skeleton.count % Joints.colors.length];
   }
 
+  static count = 0;
+
   joints() {
-    return this.landmarks;
+    return this.pose.keypoints;
   }
 
   lerp(a, b, x) {
     return (1 - x) * a + x * b;
   }
 
-  update(landmarks) {
-    this.landmarks.forEach((el, i) => {
+  reset() {
+    this.updatedLastFrame = false;
+  }
+
+  didUpdate() {
+    return this.updatedLastFrame;
+  }
+
+  update(pose) {
+    let easeFactor = 0.4;
+    this.updatedLastFrame = true;
+    this.lastUpdate = performance.now();
+    this.pose.keypoints.forEach((el, i) => {
       // lerp towards new position
-      el.x = this.lerp(el.x, landmarks[i].x, 0.2);
-      el.y = this.lerp(el.y, landmarks[i].y, 0.2);
-      el.z = this.lerp(el.z, landmarks[i].z, 0.2);
+      el.x = this.lerp(el.x, pose.keypoints[i].x, easeFactor);
+      el.y = this.lerp(el.y, pose.keypoints[i].y, easeFactor);
+      el.z = this.lerp(el.z, pose.keypoints[i].z, easeFactor);
     });
     this.lastUpdate = performance.now();
   }
 }
-*/
 
 if (window.autoInitDemo)
   window.demo = new MediapipeBodyTrackingDemo(document.body);
