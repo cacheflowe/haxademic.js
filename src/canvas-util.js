9d3ac1992addaf8a1f01bedf4d9c28feda34ca3c
Justin Gitlin <cacheflowe@cacheflowe.com>
2024-09-07 19:47:16 -0400

    Update canvas-util.js

--- src/canvas-util.js
+++ src/canvas-util.js
@@ -24,7 +24,7 @@ class CanvasUtil {
    *  Draws a filled circle. Original code from Robin W. Spencer (http://scaledinnovation.com).
    *  @use    {@code CanvasUtil.drawCircle( context, 50, 50, 40 );}
    */
-  static drawCircle(ctx, x, y, radius, extraSetup) {
+  static drawCircle(ctx, x, y, radius) {
     ctx.save();
     ctx.beginPath();
     ctx.arc(x, y, radius, 0.0, 2 * Math.PI, false);
